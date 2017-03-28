a9132495412defb478c606c7be59e4df7e106530
sergey2961 <sergey2961@gmail.com>
2017-03-28 23:41:43 +0000

    Adding destroying action

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -40,8 +40,8 @@ class ArticlesController < ApplicationController
 	def destroy
 		@article = Article.find(params[:id])
 		@article.destroy
-		#flash[:notice] = "Article was successfully deleted"
-		#redirect_to articles_path
+		flash[:notice] = "Article was successfully deleted"
+		redirect_to articles_path
 	end
 
 	private
